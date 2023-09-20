9839e9a6ae8dd8da16216ab62ca683bfc387cb19
sitjohnny <johnny.sit@uconn.edu>
2023-09-19 21:53:26 -0400

    added vars to hold some values and added some ui layouts wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { Button, useTheme } from "react-native-paper";
+import { View, TouchableWithoutFeedback, Keyboard } from "react-native";
+import {
+  Text,
+  useTheme,
+  withTheme,
+  TextInput,
+  Divider,
+  SegmentedButtons,
+  SafeAreaView,
+} from "react-native-paper";
 import { makeStyles } from "./css/App.styles";
 
-export default function App() {
+const DismissKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
+
+export function App() {
   const theme = useTheme();
+  const [checkAmount, setCheckAmount] = useState("");
+  const [numPeople, setNumPeople] = useState("");
+  const [taxPercentage, setTaxPercentage] = useState("8.875");
+  const [tipPercentage, setTipPercentage] = useState("15");
 
   const styles = makeStyles(theme);
 
   return (
-    <View style={styles.container}>
-      <Text style={{ color: "white" }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Button
+    <DismissKeyboard>
+      <View style={styles.container}>
+        <Text style={styles.title} variant="displayMedium">
+          Split The Tab
+        </Text>
+        <TextInput
+          keyboardType="numeric"
+          style={styles.textInput}
+          label="Pre-tax Amount"
+          value={checkAmount}
+          onChangeText={(value) => setCheckAmount(value)}
+          placeholder="$0.00"
+        />
+        <Divider bold={true} />
+        <TextInput
+          keyboardType="number-pad"
+          style={styles.textInput}
+          label="Number of People"
+          value={numPeople}
+          onChangeText={(value) => setNumPeople(value)}
+          placeholder="0"
+        />
+        <TextInput
+          keyboardType="numeric"
+          style={styles.textInput}
+          label="Tax Percentage"
+          value={taxPercentage}
+          onChangeText={(value) => setTaxPercentage(value)}
+        />
+
+        <SegmentedButtons
+          value={tipPercentage}
+          onValueChange={setTipPercentage}
+          style={styles.segmentedButtonInput}
+          buttons={[
+            {
+              value: "12.5",
+              label: "12.5%",
+            },
+            {
+              value: "15",
+              label: "15%",
+            },
+            { value: "18", label: "18%" },
+            { value: "", label: "Custom" },
+          ]}
+        />
+
+        {tipPercentage !== "12.5" ||
+        tipPercentage !== "15" ||
+        tipPercentage !== "18" ? (
+          <TextInput
+            keyboardType="numeric"
+            style={styles.textInput}
+            label="Tip Percentage"
+            value={tipPercentage}
+            onChangeText={(value) => setTipPercentage(value)}
+          />
+        ) : null}
+        {/* <Button
         icon="camera"
         mode="contained"
         onPress={() => console.log("Pressed")}
       >
         Hi
-      </Button>
-      <StatusBar style="auto" />
-    </View>
+      </Button> */}
+        <StatusBar style="auto" />
+      </View>
+    </DismissKeyboard>
   );
 }
+
+export default withTheme(App);
--- src/css/App.styles.js
+++ src/css/App.styles.js
@@ -3,10 +3,27 @@ import { StyleSheet } from "react-native";
 export const makeStyles = (theme) =>
   StyleSheet.create({
     container: {
-      flex: 1,
+      display: "flex",
+      flex: "1",
       backgroundColor: theme.colors.background,
       alignItems: "center",
-      justifyContent: "center",
       color: "white",
+    },
+    title: {
+      marginTop: "20%",
+    },
+    textInput: {
+      top: "5%",
+      marginBottom: "10%",
+      justifyContent: "center",
+      borderColor: "gray",
+      width: "80%",
+      borderWidth: 1,
+      borderRadius: 10,
+      padding: 10,
+    },
+    segmentedButtonInput: {
+      top: "5%",
+      width: "80%",
     },
   });
